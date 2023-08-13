4006126ca071682d383298e8be2a35fc1e44d6ba
Abdulloh <abdullohbahromjonv@gmail.com>
2023-08-13 19:02:12 +0500

    Initial Commit

---
+++ TATU litsey/TATU_litseyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TATU_litseyApp.swift
+//  TATU litsey
+//
+//  Created by Abdulloh on 13/08/23.
+//
+
+import SwiftUI
+
+@main
+struct TATU_litseyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
